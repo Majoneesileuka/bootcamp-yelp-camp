0ccb60ff17bf47b154b7b477c3d594b4e55903da
Majoneesileuka <40734532+Majoneesileuka@users.noreply.github.com>
2020-02-02 14:42:41 +0200

    Added mongodb and mongoose to keep data, added show view

--- app.js
+++ app.js
@@ -1,47 +1,88 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+// Set up requirements
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    mongoose = require("mongoose");
 
+// Connect mongodb and use bodyparser and set ejs as view enginer
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-var campgrounds = [
-    {
-        name: 'Helvetinjarvi',
-        image:
-            'https://images.unsplash.com/photo-1510312305653-8ed496efae75?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80'
-    },
-    {
-        name: 'Kasivarsi',
-        image:
-            'https://images.unsplash.com/photo-1471115853179-bb1d604434e0?ixlib=rb-1.2.1&auto=format&fit=crop&w=959&q=80'
-    },
-    {
-        name: 'Luontopolku',
-        image:
-            'https://images.unsplash.com/photo-1534950947221-dcaca2836ce8?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80'
-    }
-];
+// SCHEMA SETUP
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String,
+	description: String
+});
+
+// Create Campground model
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+/*
+Campground.create(
+{
+	name: "Granite Hill",
+	image: "https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+	description: "This is a huge granite hill, not bathrooms. No water, Beautiful granite!"
+}, function(err, campground) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Newly created Campground");
+		console.log(campground);
+	}
+});
+*/
 
 // REST routes
 app.get('/', function(req, res) {
     res.render('landing');
 });
 
+// INDEX -- Show all campgrounds
 app.get('/campgrounds', function(req, res) {
-    res.render('campgrounds', { campgrounds: campgrounds });
+	// Get all campgrounds from DB
+	Campground.find({}, function(err, allCampgrounds) {
+		if (err) {
+			console.log(err);
+		} else {
+    		res.render('index', { campgrounds: allCampgrounds });
+		}
+	});
 });
 
+// CREATE -- Add new campground to the DB
 app.post('/campgrounds', function(req, res) {
-	var name = req.body.name;
-	var image = req.body.image;
-	var newCampground = {name: name, image: image};
-	campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = { name: name, image: image, description: desc };
+    // Create a new campground and save to DB, redirect to GET /campgrounds
+    Campground.create(newCampground, function(err, newlyCreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect('/campgrounds');
+        }
+    });
 });
 
+// NEW - Show form to create new campground
 app.get('/campgrounds/new', function(req, res) {
     res.render('new');
+});
+
+// SHOW -- Show more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+	// Find the campground with provided ID and render the show page
+	Campground.findById(req.params.id, function(err, foundCampground) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("show", {campground: foundCampground});
+		}
+	});
 });
 
 // Start server
