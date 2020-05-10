77b129b9e6e197f5681d156b779f0ec949744218
Majoneesileuka <40734532+Majoneesileuka@users.noreply.github.com>
2020-05-10 14:26:05 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
 // Set up requirements
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser'),
-	mongoose = require("mongoose"),
-	flash = require("connect-flash"),
-	passport = require("passport"),
-	LocalStrategy = require("passport-local"),
-	methodOverride = require("method-override"), // To use DELETE or PUT for example
-	Campground = require("./models/campground"),
-	Comment = require("./models/comment"),
-	User = require("./models/user"),
-	seedDB = require("./seeds");
+var express = require("express"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  flash = require("connect-flash"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  methodOverride = require("method-override"), // To use DELETE or PUT for example
+  Campground = require("./models/campground"),
+  Comment = require("./models/comment"),
+  User = require("./models/user"),
+  seedDB = require("./seeds");
 
 // Require routes
 var commentRoutes = require("./routes/comments"),
-	campgroundRoutes = require("./routes/campgrounds"),
-	indexRoutes = require("./routes/index");
+  campgroundRoutes = require("./routes/campgrounds"),
+  indexRoutes = require("./routes/index");
 
 // Connect mongodb and use bodyparser and set ejs as view enginer
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v7", { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connect("mongodb://localhost:27017/yelp_camp_v7", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
@@ -29,11 +33,13 @@ app.use(flash());
 //seedDB();
 
 // Passport configuration
-app.use(require("express-session")({
-	secret: "A super secret string here!",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "A super secret string here!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -41,11 +47,11 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Pass user to every single template
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 // Use the route files
@@ -55,5 +61,5 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 
 // Start server
 app.listen(process.env.PORT || 3000, process.env.IP, function () {
-	console.log('The YelpCamp Server Has Started');
+  console.log("The YelpCamp Server Has Started");
 });
